7059977a3877119a9b6d827ab53a5578c7209278
Richard Olsson <r@richardolsson.se>
2024-02-05 11:40:54 +0100

    Add exclamation points

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send('Hello, world!');
+  res.send('Hello, world!!!!!!!!');
 });
 
 app.listen(process.env.PORT || 5080);
