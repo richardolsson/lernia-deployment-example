5dd378847a7b8d0cf0c660ec58e1806223dfdbec
Richard Olsson <r@richardolsson.se>
2025-02-03 13:10:09 +0100

    Add error feedback

--- index.js
+++ index.js
@@ -11,4 +11,10 @@ const port = process.env.PORT || 5080;
 
 console.log('Starting on port', port);
 
-app.listen(port);
+app.listen(port, '0.0.0.0', (err) => {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log('Listening!')
+    }
+});
